14b84b6fb14ceeff8e1b67b187f97161084d1135
Tim Golen <tgolen@gmail.com>
2016-04-22 10:30:00 -0600

    Setup a script to package the code

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo Current version:
+sed -n -e '/"version"/p' package.json
+
+echo What is the new version?
+read targetVersion
+
+echo Cleaning dist directory...
+rm -rf dist
+mkdir dist
+
+echo Building...
+npm run package
+
+echo Committing build
+git add .
+git commit -m "build version $targetVersion"
+
+echo Tagging release...
+git tag -a "$targetVersion" -m "$targetVersion"
