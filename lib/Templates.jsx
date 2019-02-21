80a7f1da38cf3063a329f1f8c56476d2e0339675
Marc Glasser <marc@expensify.com>
2019-02-21 15:59:57 -0800

    export a closure instead of class instance

--- lib/Templates.jsx
+++ lib/Templates.jsx
@@ -9,15 +9,8 @@ import _ from 'underscore';
  * @constructor
  */
 
-class TemplatesConstructor {
-    constructor() {
-        this.templateStore = {};
-        this.get = this.get.bind(this);
-        this.has = this.has.bind(this);
-        this.register = this.register.bind(this);
-        this.unregister = this.unregister.bind(this);
-        this.init = this.init.bind(this);
-    }
+const Templates = (function () {
+    const templateStore = {};
 
     /**
      * Returns the template
@@ -25,48 +18,12 @@ class TemplatesConstructor {
      * @param {Array} templatePath
      * @return {InlineTemplate|undefined}
      */
-    getTemplate(templatePath) {
-        let template = this.templateStore;
+    function getTemplate(templatePath) {
+        let template = templateStore;
         _.each(templatePath, (pathname) => {
             template = template[pathname];
         });
         return template;
-    }
-
-    /**
-     * Given a templatePath, return the value
-     *
-     * @param {Array} templatePath
-     * @param {Object} [data] Information that need to be injected into the template
-     * @return {String}
-     */
-    get(templatePath, data = {}) {
-        try {
-            const template = this.getTemplate(templatePath);
-            if (_.isUndefined(template)) {
-                throw Error(`Template '${templatePath}' is not defined`);
-            }
-
-            // Check for the absense of get which means someone is likely using
-            // the templating engine wrong and trying to access a template namespace
-            if (!{}.propertyIsEnumerable.call(template, 'get')) {
-                throw Error(`'${templatePath}' is not a valid template path`);
-            }
-
-            return template.get(data);
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    /**
-     * Given a templatePath, does it have a registered template?
-     * @param  {Array} templatePath
-     * @return {Boolean}
-     */
-    has(templatePath) {
-        const template = this.getTemplate(templatePath);
-        return !_.isUndefined(template);
     }
 
     /**
@@ -76,8 +33,8 @@ class TemplatesConstructor {
      * @param {Array} wantedNamespace Array of string reprensenting the namespace
      * @return {Object} Referene to the object holding the namespace
      */
-    getTemplateNamespace(wantedNamespace) {
-        let namespace = this.templateStore;
+    function getTemplateNamespace(wantedNamespace) {
+        let namespace = templateStore;
         let currentArgument;
         for (let argumentNumber = 0; argumentNumber < wantedNamespace.length; argumentNumber++) {
             currentArgument = wantedNamespace[argumentNumber];
@@ -90,92 +47,128 @@ class TemplatesConstructor {
         return namespace;
     }
 
-    /**
-     * Inits the templating engine, and slurps all DOM templates in an internal data structure
-     */
-    init() {
-        // Read the DOM to find all the templates, and make them available to the code
-        $('.js_template').each((_, $el) => {
-            const namespaceElements = $el.id.split('_');
-            const id = namespaceElements.pop();
+    return {
+        /**
+         * Given a templatePath, return the value
+         *
+         * @param {Array} templatePath
+         * @param {Object} [data] Information that need to be injected into the template
+         * @return {String}
+         */
+        get(templatePath, data = {}) {
+            try {
+                const template = getTemplate(templatePath);
+                if (_.isUndefined(template)) {
+                    throw Error(`Template '${templatePath}' is not defined`);
+                }
 
-            // remove the prefix "template" that MUST be added to have clean HTML ids
-            namespaceElements.shift();
-            const namespace = this.getTemplateNamespace(namespaceElements);
-            // eslint-disable-next-line no-use-before-define
-            namespace[id] = new DOMTemplate($el.id);
-        });
-    }
+                // Check for the absense of get which means someone is likely using
+                // the templating engine wrong and trying to access a template namespace
+                if (!{}.propertyIsEnumerable.call(template, 'get')) {
+                    throw Error(`'${templatePath}' is not a valid template path`);
+                }
 
-    /**
-     * Register a new json object in the template manager
-     *
-     * @param {Array} wantedNamespace Namespace where we want to store the templates
-     * @param {object} templateData The literal object that contain the templates
-     */
-    register(wantedNamespace, templateData) {
-        const namespace = this.getTemplateNamespace(wantedNamespace);
-        _.each(_.keys(templateData), (key) => {
-            const template = templateData[key];
-
-            if (_.isObject(template)) {
-                // If the template is an object, add templates for all keys
-                namespace[key] = {};
-                _.each(_.keys(template), (templateKey) => {
-                    // eslint-disable-next-line no-use-before-define
-                    namespace[key][templateKey] = new InlineTemplate(template[templateKey]);
-                });
-            } else {
-                // eslint-disable-next-line no-use-before-define
-                namespace[key] = new InlineTemplate(template);
+                return template.get(data);
+            } catch (err) {
+                throw err;
             }
-        });
-    }
+        },
 
-    /**
-     * Removes a namespace from the templateStore (only used for testing purposes)
-     * 
-     * @param {String} nameSpace 
-     */
-    unregister(nameSpace) {
-        delete this.templateStore[nameSpace];
-    }
+        /**
+         * Given a templatePath, does it have a registered template?
+         * @param  {Array} templatePath
+         * @return {Boolean}
+         */
+        has(templatePath) {
+            const template = getTemplate(templatePath);
+            return !_.isUndefined(template);
+        },
 
-    /**
-     * Replace the DOM HTML with the template value
-     *
-     * @param {jQuery} $target Element(s) that will be updated
-     * @param {Array} templatePath
-     * @param {Object} [data] Information that need to be injected into the template
-     */
-    insert($target, templatePath, data = {}) {
-        $target.html(this.get(templatePath, data));
-    }
+        /**
+         * Inits the templating engine, and slurps all DOM templates in an internal data structure
+         */
+        init() {
+            // Read the DOM to find all the templates, and make them available to the code
+            $('.js_template').each((__, $el) => {
+                const namespaceElements = $el.id.split('_');
+                const id = namespaceElements.pop();
 
-    /**
-     * Append the template value into a DOM elements
-     *
-     * @param {jQuery} $target Element(s) that will be updated
-     * @param {Array} templatePath
-     * @param {Object} data Information that need to be injected into the template
-     */
-    prepend($target, templatePath, data) {
-        $target.prepend(this.get(templatePath, data));
-    }
+                // remove the prefix "template" that MUST be added to have clean HTML ids
+                namespaceElements.shift();
+                const namespace = getTemplateNamespace(namespaceElements);
+                // eslint-disable-next-line no-use-before-define
+                namespace[id] = new DOMTemplate($el.id);
+            });
+        },
 
-    /**
-     * Prepend the template value into a DOM elements
-     *
-     * @param {jQuery} $target Element(s) that will be updated
-     * @param {array} templatePath
-     * @param {object} [data] Information that need to be injected into the template
-     */
-    append($target, templatePath, data) {
-        $target.append(this.get(templatePath, data));
-    }
-}
+        /**
+         * Register a new json object in the template manager
+         *
+         * @param {Array} wantedNamespace Namespace where we want to store the templates
+         * @param {object} templateData The literal object that contain the templates
+         */
+        register(wantedNamespace, templateData) {
+            const namespace = getTemplateNamespace(wantedNamespace);
+            _.each(_.keys(templateData), (key) => {
+                const template = templateData[key];
 
-const Templates = new TemplatesConstructor();
+                if (_.isObject(template)) {
+                    // If the template is an object, add templates for all keys
+                    namespace[key] = {};
+                    _.each(_.keys(template), (templateKey) => {
+                        // eslint-disable-next-line no-use-before-define
+                        namespace[key][templateKey] = new InlineTemplate(template[templateKey]);
+                    });
+                } else {
+                    // eslint-disable-next-line no-use-before-define
+                    namespace[key] = new InlineTemplate(template);
+                }
+            });
+        },
+
+        /**
+         * Removes a namespace from the templateStore (only used for testing purposes)
+         * 
+         * @param {String} nameSpace 
+         */
+        unregister(nameSpace) {
+            delete templateStore[nameSpace];
+        },
+
+        /**
+         * Replace the DOM HTML with the template value
+         *
+         * @param {jQuery} $target Element(s) that will be updated
+         * @param {Array} templatePath
+         * @param {Object} [data] Information that need to be injected into the template
+         */
+        insert($target, templatePath, data = {}) {
+            $target.html(this.get(templatePath, data));
+        },
+
+        /**
+         * Append the template value into a DOM elements
+         *
+         * @param {jQuery} $target Element(s) that will be updated
+         * @param {Array} templatePath
+         * @param {Object} data Information that need to be injected into the template
+         */
+        prepend($target, templatePath, data) {
+            $target.prepend(this.get(templatePath, data));
+        },
+
+        /**
+         * Prepend the template value into a DOM elements
+         *
+         * @param {jQuery} $target Element(s) that will be updated
+         * @param {array} templatePath
+         * @param {object} [data] Information that need to be injected into the template
+         */
+        append($target, templatePath, data) {
+            $target.append(this.get(templatePath, data));
+        }
+    };
+})();
 
 // Template Constructors
 class InlineTemplate {
