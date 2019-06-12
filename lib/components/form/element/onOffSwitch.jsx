34e2d072ca7095155c461cb44c3693ada8b94717
Rachel <rchang@expensify.com>
2019-06-12 14:50:09 -0700

    fixed ESLint errors and most  warnings

--- lib/components/form/element/onOffSwitch.jsx
+++ lib/components/form/element/onOffSwitch.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 import {invoke} from '../../../Func';
 import {UI} from '../../../CONST';
-import PropTypes from 'prop-types';
 import Switch from './switch';
-import cn from 'classnames';
 
 /**
- * An on off switch.
- *
- * To be used like this :
- * <React.c.OnOffSwitch
- *      extraClasses=""
- *      id="intacct_category"
- *      label="Categories"
- *      checked={true}
- *      description="This is it"
- *      preventEdit={true}
- *      onChangeCallback={this.onCallback}
- *      onlyShowDescriptionWhenOn={false}
- *      noLeftPaddingOnChildren={false}
- *      descriptionBeforeChildren={false}
- * >
- *      <Children />
- * </React.c.OnOffSwitch>
+ * Displays an on off switch (optionally) with label
  */
- const propTypes = {
+const propTypes = {
     id: PropTypes.string.isRequired,
-    alwaysShowChildren: PropTypes.bool,
-    labelClasses: PropTypes.any,
-    labelOnRight: PropTypes.bool,
 
     // Label to be displayed left to the switch
     label: PropTypes.string,
+
+    // True if the label should be on the right
+    labelOnRight: PropTypes.bool,
+
+    // Classes of the label
+    labelClasses: PropTypes.any,
 
     // True if the switch is on
     checked: PropTypes.bool.isRequired,
@@ -54,10 +41,13 @@ import cn from 'classnames';
     // If set to true, children elements won't be applied a left padding (optional)
     noLeftPaddingOnChildren: PropTypes.bool,
 
+    // Whether we should always show the children classes
+    alwaysShowChildren: PropTypes.bool,
+
     // If set to true, the description of the switch will be placed above the children elements (optional)
     descriptionBeforeChildren: PropTypes.bool,
 
-    //
+    // Safety check to ensure we are using dangerouslySetInnerHTML() safely
     safeDescription: PropTypes.bool,
 
     // An array of extra classes to put on the combobox
@@ -69,13 +59,23 @@ import cn from 'classnames';
 };
 
 const defaultProps = {
-    checked: false,
+    label: '',
+    labelOnRight: false,
+    labelClasses: null,
+    onChangeCallback: () => {},
+    children: null,
+    preventEdit: false,
+    description: '',
+    onlyShowDescriptionWhenOn: false,
+    noLeftPaddingOnChildren: false,
+    alwaysShowChildren: false,
+    descriptionBeforeChildren: false,
     safeDescription: false,
     extraClasses: [],
 };
 
 class onOffSwitch extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
 
         this.getInitialState = this.getInitialState.bind(this);
@@ -121,7 +121,7 @@ class onOffSwitch extends React.Component {
      */
     lock(checked, msg) {
         this.setState({
-            checked: checked,
+            checked,
             preventEdit: true,
             preventEditDescription: msg
         });
@@ -131,61 +131,89 @@ class onOffSwitch extends React.Component {
      * Release switch to its original value, and allow editing it
      */
     unlock() {
-        this.setState({
-            checked: this.state.checked,
+        this.setState(prevState => ({
+            checked: prevState.checked,
             preventEdit: false,
             preventEditDescription: ''
-        });
+        }));
     }
 
     render() {
-        console.log('~~HELLOOOO from js-libs');
         const childrenClasses = {
             depreciated: !this.props.noLeftPaddingOnChildren,
             helperLabel: !this.props.noLeftPaddingOnChildren
         };
+
         if (!this.props.alwaysShowChildren) {
             childrenClasses[UI.HIDDEN] = !this.state.checked;
         }
 
-        const children = this.props.children
-            ? <div className={cn(childrenClasses)}>{this.props.children}</div>
-            : null;
+        const children = this.props.children ? (
+            <div className={cn(childrenClasses)}>{this.props.children}</div>
+        ) : null;
 
-        // Only use dangerouslySetInnerHTML if we explicitly pass safeDescription as a parameter; otherwise, just set the description inside a div
-        // We use dangerouslySetInnerHTML, which could introduce XSS vulnerabilities if derived from user input, to keep the description property simple without a bunch of nested HTML elements
-        const description = this.state.preventEdit && this.state.preventEditDescription ? this.state.preventEditDescription : this.props.description;
+        // Only use dangerouslySetInnerHTML if we explicitly pass safeDescription as a parameter.
+        // Otherwise, just set the description inside a div
+        // We use dangerouslySetInnerHTML, which could introduce XSS vulnerabilities if derived from user input,
+        // to keep the description property simple without a bunch of nested HTML elements
+        const description = this.state.preventEdit && this.state.preventEditDescription
+            ? this.state.preventEditDescription
+            : this.props.description;
         let descriptionElm = null;
+
         if (description && (this.state.checked || !this.props.onlyShowDescriptionWhenOn)) {
             if (this.props.safeDescription) {
-                descriptionElm = <div className={'depreciated helperLabel'} dangerouslySetInnerHTML={{__html: description}} />;
+                descriptionElm = (
+                    <div
+                        className="depreciated helperLabel"
+                        dangerouslySetInnerHTML={{__html: description}}
+                    />
+                );
             } else {
-                descriptionElm = <div className={'depreciated helperLabel'}>{description}</div>;
+                descriptionElm = <div className="depreciated helperLabel">{description}</div>;
             }
         }
 
         return (
             <div>
-                {this.props.label && !this.props.labelOnRight ? <label className={cn(this.props.labelClasses)} htmlFor={this.props.id}>{this.props.label}</label> : null}
+                {/* For 100% a11y compliance we'd need to move the <input> into the <label> element */}
+                {/* eslint-disable jsx-a11y/label-has-for */}
+                {this.props.label && !this.props.labelOnRight
+                    ? <label className={cn(this.props.labelClasses)} htmlFor={this.props.id}>{this.props.label}</label>
+                    : null}
                 <Switch
                     id={this.props.id}
                     checked={this.state.checked}
                     disabled={this.state.preventEdit}
                     onChange={this.onChangeCallback}
-                    extraClasses={[cn('onoffswitch-wrapper', this.props.extraClasses), !this.props.labelOnRight ? 'marginLeft10' : '']}
+                    extraClasses={[
+                        cn('onoffswitch-wrapper', this.props.extraClasses),
+                        !this.props.labelOnRight ? 'marginLeft10' : ''
+                    ]}
                 />
-                {this.props.label && this.props.labelOnRight ? <label className={cn(this.props.labelClasses, 'marginLeft5')} htmlFor={this.props.id}>{this.props.label}</label> : null}
-                {this.props.descriptionBeforeChildren ? <div>
-                    {descriptionElm}
-                    {children}
-                </div> : <div>
-                    {children}
-                    {descriptionElm}
-                </div>}
+                {this.props.label && this.props.labelOnRight ? (
+                    <label
+                        className={cn(this.props.labelClasses, 'marginLeft5')}
+                        htmlFor={this.props.id}
+                    >
+                        {this.props.label}
+                    </label>
+                ) : null}
+                {this.props.descriptionBeforeChildren ? (
+                    <div>
+                        {descriptionElm}
+                        {children}
+                    </div>
+                ) : (
+                    <div>
+                        {children}
+                        {descriptionElm}
+                    </div>
+                )}
             </div>
         );
     }
-};
+}
 
 onOffSwitch.propTypes = propTypes;
 onOffSwitch.defaultProps = defaultProps;
