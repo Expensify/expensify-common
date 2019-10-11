dec3c42c3434ab2c91a1e5f5b6f50e153444ca75
Marc Glasser <marc@expensify.com>
2019-10-10 18:24:37 -0700

    add methods from master

--- lib/ExpenseRule.jsx
+++ lib/ExpenseRule.jsx
@@ -34,4 +34,45 @@ export default class ExpenseRule {
     getExternalTaxID() {
         return this.tax.field_id_TAX.externalID || '';
     }
+
+    /**
+     * Checks if the passed expense matches this expense rule
+     *
+     * @param {SExpense3} expense
+     * @returns {boolean}
+     */
+    isMatch(expense) {
+        let isMatch = true;
+        _.each(this.applyWhen, (conditions) => {
+            switch (conditions.field) {
+                case 'category':
+                    if (!this.checkCondition(conditions.condition, conditions.value, expense.getCategory())) {
+                        isMatch = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return isMatch;
+    }
+
+    /**
+     * Checks the passed value against the actual based on what the condition (matches, greater, less than, etc.)
+     *
+     * @param {string} condition
+     * @param {Mixed} ruleValue
+     * @param {Mixed} transactionValue
+     * @returns {boolean}
+     */
+    checkCondition(condition, ruleValue, transactionValue) {
+        // Add more condition types (Greater than, Less than, Contains) below
+        switch (condition) {
+            case 'matches':
+                return ruleValue === transactionValue;
+            default:
+                return false;
+        }
+    }
 };
