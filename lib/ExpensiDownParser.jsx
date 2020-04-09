b3e5f04771cec4fefa7bfdca0d3ff67fc8a637c8
Joe Gambino <joe@expensify.com>
2020-04-09 15:17:02 -0700

    add comments to explain \b and \B

--- lib/ExpensiDownParser.jsx
+++ lib/ExpensiDownParser.jsx
@@ -13,14 +13,16 @@ export default class ExpensiDownParser {
                 replacement: '$1<a href="$2" target="_blank">$2</a>$1',
             },
             {
-                name: 'bold',
-                regex: '\\B\\*(.*?)\\*\\B',
-                replacement: '<strong>$1</strong>'
-            },
-            {
+                // Use \b in this case because it will match on words, letters, and _: https://www.rexegg.com/regex-boundaries.html#wordboundary
                 name: 'italic',
                 regex: '\\b\\_(.*?)\\_\\b',
                 replacement: '<em>$1</em>'
+            },
+            {
+                // Use \B in this case because \b doesn't match * or ~. \B will match everything that \b doesn't, so it works for * and ~: https://www.rexegg.com/regex-boundaries.html#notb
+                name: 'bold',
+                regex: '\\B\\*(.*?)\\*\\B',
+                replacement: '<strong>$1</strong>'
             },
             {
                 name: 'strikethrough',
