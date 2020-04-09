922fe32d144d89d5038cafee279d2c819bdf4950
Joe Gambino <joe@expensify.com>
2020-04-09 15:02:22 -0700

    update regex to include punctuation, remove spaces

--- lib/ExpensiDownParser.jsx
+++ lib/ExpensiDownParser.jsx
@@ -2,7 +2,7 @@ export default class ExpensiDownParser {
     constructor() {
         /**
          * The list of regex replacements to do on a comment. Check the link regex is first so links are processed
-         * before other delimeters
+         * before other delimiters
          *
          * @type {Object[]}
          */
@@ -14,31 +14,25 @@ export default class ExpensiDownParser {
             },
             {
                 name: 'bold',
-                regex: '(?:^|\\s)(\\*)(.*?)\\1(?:\\s|$)',
-                replacement: ' <strong>$2</strong> '
+                regex: '\\B\\*(.*?)\\*\\B',
+                replacement: '<strong>$1</strong>'
             },
             {
                 name: 'italic',
-                regex: '(?:^|\\s)(\\_)(.*?)\\1(?:\\s|$)',
-                replacement: ' <em>$2</em> '
+                regex: '\\b\\_(.*?)\\_\\b',
+                replacement: '<em>$1</em>'
             },
             {
                 name: 'strikethrough',
-                regex: '(?:^|\\s)(\\~)(.*?)\\1(?:\\s|$)',
-                replacement: ' <del>$2</del> '
+                regex: '\\B\\~(.*?)\\~\\B',
+                replacement: '<del>$1</del>'
             }
         ];
     }
 
     replace(text) {
         this.rules.forEach((rule) => {
-            /**
-             * The replacements for bold/italic/strikethrough have spaces added on the front and back of the replacement
-             * value (since we're capturing those spaces), so make sure we trim the string after the replacement is done
-             *
-             * We want to capture those spaces so that we capture " *test* " but not "test*test*test"
-             */
-            text = text.replace(new RegExp(rule.regex, "g"), rule.replacement).trim();
+            text = text.replace(new RegExp(rule.regex, "g"), rule.replacement);
         });
 
         return text;
