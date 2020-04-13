33485e0aa388a32926cddc148cd22eff43ae77b2
Joe Gambino <joe@expensify.com>
2020-04-13 14:49:18 -0700

    fix regex for combination tests

--- lib/ExpensiMark.jsx
+++ lib/ExpensiMark.jsx
@@ -1,4 +1,4 @@
-export default class ExpensiDownParser {
+export default class ExpensiMark {
     constructor() {
         /**
          * The list of regex replacements to do on a comment. Check the link regex is first so links are processed
@@ -9,13 +9,17 @@ export default class ExpensiDownParser {
         this.rules = [
             {
                 name: 'link',
-                regex: '([_*~]*?)(((?:https?):\\/\\/|www\\.)?[^\\s<>"\'´.-][^\\s<>"\'´]*?\\.[a-z\\d.]+[^\\s<>"\']*)\\1',
+                regex: '([_*~]*?)(((?:https?):\\/\\/|www\\.)?[^\\s<>*~_"\'´.-][^\\s<>"\'´]*?\\.[a-z\\d.]+[^\\s<>*~"\']*)\\1',
                 replacement: '$1<a href="$2" target="_blank">$2</a>$1',
             },
             {
-                // Use \b in this case because it will match on words, letters, and _: https://www.rexegg.com/regex-boundaries.html#wordboundary
+                /**
+                 * Use \b in this case because it will match on words, letters, and _: https://www.rexegg.com/regex-boundaries.html#wordboundary
+                 * The !_blank is to prevent the `target="_blank">` seciton of the link replacement from being captured
+                 * Additionally, something like `\b\_([^<>]*?)\_\b` doesn't work because it won't replace `_https://www.test.com_`
+                 */
                 name: 'italic',
-                regex: '\\b\\_(.*?)\\_\\b',
+                regex: '(?!_blank\">)\\b\\_(.*?)\\_\\b',
                 replacement: '<em>$1</em>'
             },
             {
