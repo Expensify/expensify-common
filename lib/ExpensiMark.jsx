6aae07f991c0ec24cd95f8a806ef8884b0a28205
Andre Fonseca <afonseca@expensify.com>
2020-04-24 10:05:15 +0100

    remove the parens

--- lib/ExpensiMark.jsx
+++ lib/ExpensiMark.jsx
@@ -37,7 +37,7 @@ export default class ExpensiMark {
             },
             {
                 name: 'newline',
-                regex: '(\\n)',
+                regex: '\\n',
                 replacement: '<br>',
             }
         ];
