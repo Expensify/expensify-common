6d4db231714ad377ac69a75e03f3081cb7790257
Rory Abraham <rory@expensify.com>
2021-03-04 21:39:13 -0800

    Get deploy blockers created correctly

--- __tests__/GithubUtils-test.js
+++ __tests__/GithubUtils-test.js
@@ -91,7 +91,7 @@ describe('GithubUtils', () => {
                 ['https://github.com/Expensify/expensify-common/pull/346', '346'],
             ])('getPullRequestNumberFromURL("%s")', (input, expected) => {
                 expect(githubUtils.getPullRequestNumberFromURL(input)).toBe(expected);
-            })
+            });
         });
 
         describe('invalid pull requests', () => {
@@ -102,8 +102,62 @@ describe('GithubUtils', () => {
             ])('getPullRequestNumberFromURL("%s")', (input) => {
                 expect(() => githubUtils.getPullRequestNumberFromURL(input))
                     .toThrow(new Error(`Provided URL ${input} is not a Github Pull Request!`));
-            })
-        })
+            });
+        });
+    });
+
+    describe('getIssueNumberFromURL', () => {
+        const octokit = new Octokit();
+        const githubUtils = new GithubUtils(octokit);
+
+        describe('valid issues', () => {
+            test.each([
+                ['https://github.com/Expensify/Expensify/issues/156369', '156369'],
+                ['https://github.com/Expensify/Expensify.cash/issues/1644', '1644'],
+                ['https://github.com/Expensify/expensify-common/issues/346', '346'],
+            ])('getIssueNumberFromURL("%s")', (input, expected) => {
+                expect(githubUtils.getIssueNumberFromURL(input)).toBe(expected);
+            });
+        });
+
+        describe('invalid issues', () => {
+            test.each([
+                ['https://www.google.com/'],
+                ['https://github.com/Expensify/Expensify/pull/156481'],
+                ['https://docs.google.com/document/d/1mMFh-m1seOES48r3zNqcvfuTvr3qOAsY6n5rP4ejdXE/edit?ts=602420d2#'],
+            ])('getIssueNumberFromURL("%s")', (input) => {
+                expect(() => githubUtils.getIssueNumberFromURL(input))
+                    .toThrow(new Error(`Provided URL ${input} is not a Github Issue!`));
+            });
+        });
+    });
+
+    describe('getIssueOrPullRequestNumberFromURL', () => {
+        const octokit = new Octokit();
+        const githubUtils = new GithubUtils(octokit);
+
+        describe('valid issues and pull requests', () => {
+            test.each([
+                ['https://github.com/Expensify/Expensify/issues/156369', '156369'],
+                ['https://github.com/Expensify/Expensify.cash/issues/1644', '1644'],
+                ['https://github.com/Expensify/expensify-common/issues/346', '346'],
+                ['https://github.com/Expensify/Expensify/pull/156369', '156369'],
+                ['https://github.com/Expensify/Expensify.cash/pull/1644', '1644'],
+                ['https://github.com/Expensify/expensify-common/pull/346', '346'],
+            ])('getIssueOrPullRequestNumberFromURL("%s")', (input, expected) => {
+                expect(githubUtils.getIssueOrPullRequestNumberFromURL(input)).toBe(expected);
+            });
+        });
+
+        describe('invalid issues/pull requests', () => {
+            test.each([
+                ['https://www.google.com/'],
+                ['https://docs.google.com/document/d/1mMFh-m1seOES48r3zNqcvfuTvr3qOAsY6n5rP4ejdXE/edit?ts=602420d2#'],
+            ])('getIssueOrPullRequestNumberFromURL("%s")', (input) => {
+                expect(() => githubUtils.getIssueOrPullRequestNumberFromURL(input))
+                    .toThrow(new Error(`Provided URL ${input} is not a Github Issue or Pull Request!`));
+            });
+        });
     });
 
     describe('generateStagingDeployCashBody', () => {
@@ -118,6 +172,11 @@ describe('GithubUtils', () => {
             'https://github.com/Expensify/Expensify/pull/3',
             'https://github.com/Expensify/Expensify/pull/3',
             'https://github.com/Expensify/Expensify/pull/1',
+        ];
+
+        const baseDeployBlockerList = [
+            'https://github.com/Expensify/Expensify/pull/3',
+            'https://github.com/Expensify/Expensify/issues/4',
         ];
 
         // eslint-disable-next-line max-len
@@ -136,21 +195,27 @@ describe('GithubUtils', () => {
             expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, [basePRList[0]])).toBe(`${baseExpectedOutput}${openCheckbox} ${basePRList[3]}\r\n${closedCheckbox} ${basePRList[0]}\r\n${openCheckbox} ${basePRList[1]}\r\n`);
         });
 
+        // eslint-disable-next-line max-len
+        const allVerifiedExpectedOutput = `${baseExpectedOutput}${closedCheckbox} ${basePRList[3]}\r\n${closedCheckbox} ${basePRList[0]}\r\n${closedCheckbox} ${basePRList[1]}\r\n`;
         test('Test all verified PRs', () => {
-            // eslint-disable-next-line max-len
-            expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, basePRList)).toBe(`${baseExpectedOutput}${closedCheckbox} ${basePRList[3]}\r\n${closedCheckbox} ${basePRList[0]}\r\n${closedCheckbox} ${basePRList[1]}\r\n`);
+            expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, basePRList))
+                .toBe(allVerifiedExpectedOutput);
         });
 
+        const deployBlockerHeader = '\r\n**Deploy Blockers:**\r\n';
         test('Test no resolved deploy blockers', () => {
-
+            expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, basePRList, baseDeployBlockerList))
+                .toBe(`${allVerifiedExpectedOutput}${deployBlockerHeader}${openCheckbox} ${baseDeployBlockerList[0]}\r\n${openCheckbox} ${baseDeployBlockerList[1]}\r\n`);
         });
 
         test('Test some resolved deploy blockers', () => {
-
+            expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, basePRList, baseDeployBlockerList, [baseDeployBlockerList[0]]))
+                .toBe(`${allVerifiedExpectedOutput}${deployBlockerHeader}${closedCheckbox} ${baseDeployBlockerList[0]}\r\n${openCheckbox} ${baseDeployBlockerList[1]}\r\n`);
         });
 
         test('Test all resolved deploy blockers', () => {
-
+            expect(githubUtils.generateStagingDeployCashBody(tag, basePRList, basePRList, baseDeployBlockerList, baseDeployBlockerList))
+                .toBe(`${allVerifiedExpectedOutput}${deployBlockerHeader}${closedCheckbox} ${baseDeployBlockerList[0]}\r\n${closedCheckbox} ${baseDeployBlockerList[1]}\r\n`)
         });
     })
 })
