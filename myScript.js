b4d6f5e05f6c49c01bc347f11754cd5c4fcf0503
tugbadogan <tugbadogan92@gmail.com>
2021-03-06 15:34:47 +0000

    Add some github actions for testing code

---
+++ myScript.js
@@ -0,0 +1,9 @@
+import GitUtils from './lib/GitUtils';
+
+// Then hard-code your test usages here and console.log the results
+const fromRef = '9254b614399175a6a85745a99a6fb5bb9789915d';
+const toRef = '07bc78b5932dd94e2576227d5dc89b6017877186';
+const gitUtils = new GitUtils()
+gitUtils.getPullRequestsMergedBetween(fromRef, toRef)
+    .then(pullRequestNumbers => console.log(pullRequestNumbers))
+    .catch(err => console.log(err));
