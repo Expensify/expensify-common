de020e564dbcc6fffc5c3a92ee1097acc7a6cdba
tugbadogan <tugbadogan92@gmail.com>
2021-03-07 16:33:12 +0000

    Addressing comments from roryabraham
    Added test getPullRequestsMergedBetween by mocking exec.
    Used matchAll instead of match for regex search
    Used CJS import in module

--- lib/GitUtils.jsx
+++ lib/GitUtils.jsx
@@ -1,7 +1,7 @@
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
-export default class GitUtils {
+module.exports = class GitUtils {
 
     /**
      * Takes in two git refs and returns a list of PR numbers of all PRs merged between those two refs
@@ -11,34 +11,11 @@ export default class GitUtils {
      * @returns {Promise}
      */
     getPullRequestsMergedBetween(fromRef, toRef) {
-        return new Promise((resolve, reject) => {
-            exec(`git log --format="%s" ${fromRef}...${toRef}`)
-                .then(({ stdout, stderr }) => {
-                    const commitMessages = stdout.split('\n');
-                    const pullRequestNumbers = commitMessages
-                        .map(commitMessage => this.getPullRequestNumberFromCommitMessage(commitMessage))
-                        .filter(prNumber => prNumber != null);
-                    resolve(pullRequestNumbers);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
-        });
-    }
-
-    /**
-     * Takes in a git commit message and returns pull request number 
-     * if it is merge commit or null
-     * 
-     * @param {String} commitMessage
-     * @returns {String}
-     */
-    getPullRequestNumberFromCommitMessage(commitMessage) {
-        const regExp = new RegExp(/Merge pull request #(\d{1,6})/);
-        const match = commitMessage.match(regExp);
-        if (match == null) {
-            return null;
-        }
-        return match[1];
+        return exec(`git log --format="%s" ${fromRef}...${toRef}`)
+            .then(({ stdout, stderr }) => {
+                const pullRequestNumbers = [...stdout.matchAll(new RegExp(/Merge pull request #(\d{1,6})/, 'g'))]
+                    .map(match => match[1]);
+                return pullRequestNumbers;
+            })
     }
 }
