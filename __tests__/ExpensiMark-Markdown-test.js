2a718f0772ac76eb52241bbc5ed36dab75521a57
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-07-30 00:42:21 +0530

    quote improvement

--- __tests__/ExpensiMark-Markdown-test.js
+++ __tests__/ExpensiMark-Markdown-test.js
@@ -118,9 +118,12 @@ test('Test HTML string with encoded entities', () => {
 
 test('Test HTML string with blockquote', () => {
     const testString = '<blockquote><p>This GH seems to assume that there will be something in the paste\nbuffer when you copy block-quoted text out of slack. But when I dump\nsome <em>lorem ipsum</em> into a blockquote in Slack, copy it to the\nbuffer, then dump it into terminal, there\'s nothing. And if I dump it </blockquote>'
-    + '<blockquote>line1\nline2\n\nsome <em>lorem ipsum</em> into a blockquote in Slack, copy it to the\n\n\nbuffer </blockquote>';
+    + '<blockquote>line1\nline2\n\nsome <em>lorem ipsum</em> into a blockquote in Slack, copy it to the\n\n\nbuffer </blockquote>'
+    + '<blockquote style="color:red;" data-label="note">line1 <em>lorem ipsum</em></blockquote>';
+
     const resultString = '\n> This GH seems to assume that there will be something in the paste\n> buffer when you copy block-quoted text out of slack. But when I dump\n> some _lorem ipsum_ into a blockquote in Slack, copy it to the\n> buffer, then dump it into terminal, there\'s nothing. And if I dump it\n'
-    + '\n> line1\n> line2\n> \n> some _lorem ipsum_ into a blockquote in Slack, copy it to the\n> \n> \n> buffer\n';
+    + '\n> line1\n> line2\n> \n> some _lorem ipsum_ into a blockquote in Slack, copy it to the\n> \n> \n> buffer\n'
+    + '\n> line1 _lorem ipsum_\n';
 
     expect(parser.htmlToMarkdown(testString)).toBe(resultString);
 });
