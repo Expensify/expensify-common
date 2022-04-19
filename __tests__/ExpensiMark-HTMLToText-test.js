958202dc6a32e948af979e0bd40344fc1f2baf80
Mohammad Luthfi Fathur Rahman <luthfi.ufi14@gmail.com>
2022-04-20 02:17:14 +0700

    updated function name

--- __tests__/ExpensiMark-HTMLToText-test.js
+++ __tests__/ExpensiMark-HTMLToText-test.js
@@ -7,19 +7,19 @@ test('Test space replacement on breakline', () => {
     const html = '1<br />2<br />3';
     const text = '1 2 3';
 
-    expect(parser.htmlToFlatText(html)).toBe(text);
+    expect(parser.htmlToText(html)).toBe(text);
 });
 
 test('Test space replacement on blockquote close tag', () => {
     const html = '<blockquote>Confusing stuff</blockquote>Tell me about it';
     const text = 'Confusing stuff Tell me about it';
 
-    expect(parser.htmlToFlatText(html)).toBe(text);
+    expect(parser.htmlToText(html)).toBe(text);
 });
 
 test('Test replacement on html', () => {
     const html = 'First Line<br /><blockquote>Quoted line <code>code</code></blockquote>3<br /><blockquote>4</blockquote>Five';
     const text = 'First Line Quoted line code 3 4 Five';
 
-    expect(parser.htmlToFlatText(html)).toBe(text);
+    expect(parser.htmlToText(html)).toBe(text);
 });
