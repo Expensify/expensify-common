c89e6a1e2ddf443956984b861f4f1077bd00368b
Aldo Canepa <aldo@expensify.com>
2022-11-28 16:26:41 -0800

    Replace end quote by new line

--- __tests__/ExpensiMark-HTMLToText-test.js
+++ __tests__/ExpensiMark-HTMLToText-test.js
@@ -19,14 +19,14 @@ test('Test space replacement on consecutive breaklines', () => {
 
 test('Test space replacement on blockquote close tag', () => {
     const html = '<blockquote>Confusing stuff</blockquote>Tell me about it';
-    const text = 'Confusing stuff Tell me about it';
+    const text = 'Confusing stuff\nTell me about it';
 
     expect(parser.htmlToText(html)).toBe(text);
 });
 
 test('Test replacement on html', () => {
     const html = 'First Line<br /><blockquote>Quoted line <code>code</code></blockquote>3<br /><blockquote>4</blockquote>Five';
-    const text = 'First Line\nQuoted line code 3\n4 Five';
+    const text = 'First Line\nQuoted line code\n3\n4\nFive';
 
     expect(parser.htmlToText(html)).toBe(text);
 });
