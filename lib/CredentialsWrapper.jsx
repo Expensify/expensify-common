940cb67e546dab9df092fc8dad700e79a7d5b400
Vivek Kumar <vivek@expensify.com>
2023-01-05 13:51:40 +0530

    exported LOGIN_PARTNER_DETAILS as we will need PARTNER_NAME/PASSWORD to fetch new token

--- lib/CredentialsWrapper.jsx
+++ lib/CredentialsWrapper.jsx
@@ -1,6 +1,6 @@
 import localForage from 'localforage';
 
-const LOGIN_PARTNER_DETAILS = {
+export const LOGIN_PARTNER_DETAILS = {
     CREDENTIALS_KEY: 'DEVICE_SESSION_CREDENTIALS',
     EXPENSIFY_PARTNER_PREFIX: 'expensify.',
     PARTNER_NAME: 'chat-expensify-com',
@@ -34,7 +34,7 @@ const CredentialWrapper = {
      * @returns {Promise} Promise resolves to an object containing partnerUserID and partnerUserSecret
      */
     setCredentials(credentials) {
-        if (!credentials.partnerUserID || credentials.partnerUserSecret) {
+        if (!credentials.partnerUserID || !credentials.partnerUserSecret) {
             return Promise.reject('Invalid credential pair');
         }
 
