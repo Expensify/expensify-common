847f50e6cbad5ddcf72c4c8e13a962e3a2f17260
Sibtain Ali <allroundexperts@gmail.com>
2023-04-21 15:02:55 +0500

    Update URL-test.js
    Removed more spaces

--- __tests__/URL-test.js
+++ __tests__/URL-test.js
@@ -6,18 +6,14 @@ describe('Mandatory protocol for URL', () => {
         expect(regexToTest.test('https://google.com/')).toBeTruthy();
         expect(regexToTest.test('http://google.com/')).toBeTruthy();
         expect(regexToTest.test('ftp://google.com/')).toBeTruthy();
-
         expect(regexToTest.test('https://we.are.expensify.com/how-we-got-here')).toBeTruthy();
-
         expect(regexToTest.test('https://google.com:12')).toBeTruthy();
         expect(regexToTest.test('https://google.com:65535')).toBeTruthy();
-
         expect(regexToTest.test('https://google.com:65535/path/my')).toBeTruthy();
     });
     it('correctly tests invalid urls', () => {
         const regexToTest = new RegExp(`^${URL_REGEX_WITH_REQUIRED_PROTOCOL}$`, 'i');
         expect(regexToTest.test('google.com')).toBeFalsy();
-
         expect(regexToTest.test('https://google.com:02')).toBeFalsy();
         expect(regexToTest.test('https://google.com:65536')).toBeFalsy();
     });
@@ -29,17 +25,13 @@ describe('Optional protocol for URL', () => {
         expect(regexToTest.test('google.com/')).toBeTruthy();
         expect(regexToTest.test('https://google.com/')).toBeTruthy();
         expect(regexToTest.test('ftp://google.com/')).toBeTruthy();
-
         expect(regexToTest.test('we.are.expensify.com/how-we-got-here')).toBeTruthy();
-
         expect(regexToTest.test('google.com:12')).toBeTruthy();
         expect(regexToTest.test('google.com:65535')).toBeTruthy();
-
         expect(regexToTest.test('google.com:65535/path/my')).toBeTruthy();
     });
     it('correctly tests invalid urls', () => {
         const regexToTest = new RegExp(`^${URL_REGEX}$`, 'i');
-
         expect(regexToTest.test('google.com:02')).toBeFalsy();
         expect(regexToTest.test('google.com:65536')).toBeFalsy();
     });
