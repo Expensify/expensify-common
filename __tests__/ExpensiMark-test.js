6c4a3d15a93da160d3d94f98fca8d3dec4314931
Eric Han <hanyl_fight@126.com>
2023-07-23 23:38:43 +0800

    add test for try/catch change for methods

--- __tests__/ExpensiMark-test.js
+++ __tests__/ExpensiMark-test.js
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
 import ExpensiMark from '../lib/ExpensiMark';
+import Str from '../lib/str';
+import _ from 'underscore';
 
 const parser = new ExpensiMark();
 
@@ -13,4 +15,21 @@ test('Test text is unescaped', () => {
     const htmlString = '&amp;&#32;&amp;amp;&#32;&amp;lt;&#32;&lt;';
     const resultString = '& &amp; &lt; <';
     expect(parser.htmlToText(htmlString)).toBe(resultString);
+});
+
+test('Test with regex Maximum regex stack depth reached error', () => {
+    const testString = '<h1>heading</h1> asjjssjdjdjdjdjdjjeiwiwiwowkdjdjdieikdjfidekjcjdkekejdcjdkeekcjcdidjjcdkekdiccjdkejdjcjxisdjjdkedncicdjejejcckdsijcjdsodjcicdkejdicdjejajasjjssjdjdjdjdjdjjeiwiwiwowkdjdjdieikdjfisjksksjsjssskssjskskssksksksksskdkddkddkdksskskdkdkdksskskskdkdkdkdkekeekdkddenejeodxkdndekkdjddkeemdjxkdenendkdjddekjcjdkekejdcjdkeekcjcdidjjcdkekdiccjdkejdjcjxisdjjdkedncicdjejejcckdsijcjdsodjcicdkejdicdjejajasjjssjdjdjdjdjdjjeiwiwiwowkdjdjdieikdjfidekjcjdkekejdcjdkeekcjcdidjjcdkekdiccjdkejdjcjxisdjjdkedncicdjejejcckdsijcjdsodjcicdkejdi.cdjd';
+    const parser = new ExpensiMark();
+    // Mock method modifyTextForUrlLinks to let it throw an error to test try/catch of method ExpensiMark.replace
+    const modifyTextForUrlLinksMock = jest.fn((a, b, c) => {throw new Error('Maximum regex stack depth reached')});
+    parser.modifyTextForUrlLinks = modifyTextForUrlLinksMock;
+    expect(parser.replace(testString)).toBe(_.escape(testString));
+    expect(modifyTextForUrlLinksMock).toHaveBeenCalledTimes(1);
+
+    // Mock method extractLinksInMarkdownComment to let it return undefined to test try/catch of method ExpensiMark.extractLinksInMarkdownComment
+    const extractLinksInMarkdownCommentMock = jest.fn((a) => undefined);
+    parser.extractLinksInMarkdownComment = extractLinksInMarkdownCommentMock;
+    expect(parser.extractLinksInMarkdownComment(testString)).toBe(undefined);
+    expect(parser.getRemovedMarkdownLinks(testString, 'google.com')).toStrictEqual([]);
+    expect(extractLinksInMarkdownCommentMock).toHaveBeenCalledTimes(3);
 });
