37f4eaf9cb658e2bb04e41ab17f0259e030c13d3
Andrew Rosiclair <arosiclair@expensify.com>
2023-10-19 11:22:54 -0400

    add getAvatarForEmail

--- lib/API.jsx
+++ lib/API.jsx
@@ -318,6 +318,18 @@ export default function API(network, args) {
         },
 
         /**
+         * @param {Object} parameters
+         * @param {String[]} parameters.emailList
+         *
+         * @returns {ExpensifyAPIDeferred}
+         */
+        getAvatarForEmail(parameters) {
+            const commandName = 'GetAvatarForEmail';
+            requireParameters(['email'], parameters, commandName);
+            return performPOSTRequest(commandName, parameters);
+        },
+
+        /**
          * Performs API command "Get"
          *
          * @param {Object} parameters The API call parameters, must contain "returnValueList"
