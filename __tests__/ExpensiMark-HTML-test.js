a45eda64d45d9bc8c2fdff9f1988ffa5d9befdfa
akamefi202 <akamefi202@gmail.com>
2023-11-22 08:45:51 -0500

    update comment & test case
    Signed-off-by: akamefi202 <akamefi202@gmail.com>

--- __tests__/ExpensiMark-HTML-test.js
+++ __tests__/ExpensiMark-HTML-test.js
@@ -1702,10 +1702,4 @@ describe('when should keep whitespace flag is enabled', () => {
 test('Test code fence within inline code', () => {
     let testString = '`(```test```)`';
     expect(parser.replace(testString)).toBe('&#x60;(<pre>test</pre>)&#x60;');
-
-    testString = '`test`space```block```';
-    expect(parser.replace(testString)).toBe('<code>test</code>space<pre>block</pre>');
-    
-    testString = '```block```space`test`';
-    expect(parser.replace(testString)).toBe('<pre>block</pre>space<code>test</code>');
 });
