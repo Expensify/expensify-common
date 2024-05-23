7b7956678705abc7ff3f084f8e809a9878714ed9
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-05-23 15:54:41 +0200

    migrate Num.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
     extends: ['expensify', 'prettier'],
     rules: {
         // Allow JSX to be written in any file ignoring the extension type
-        'react/jsx-filename-extension': 'off'
+        'react/jsx-filename-extension': 'off',
+        'no-restricted-globals': 'off',
     },
     plugins: ['jest'],
     env: {
-        "jest/globals": true
-    }
+        'jest/globals': true,
+    },
 };
