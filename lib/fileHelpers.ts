3b69f123df09d557c92bb5a028a3d23d7e123bcf
dominictb <tb-dominic@outlook.com>
2024-06-12 00:01:17 +0700

    chore: move fileHelpers to TS
    Signed-off-by: dominictb <tb-dominic@outlook.com>

--- lib/fileHelpers.ts
+++ lib/fileHelpers.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import * as Constants from "./CONST"
 
-const isVideoFile = (fileName) => {
+const isVideoFile = (fileName: string) => {
   const lowerCaseFileName =  fileName.toLowerCase()
   return _.some(Constants.CONST.VIDEO_EXTENSIONS, (ext => lowerCaseFileName.endsWith(ext)))
 }
