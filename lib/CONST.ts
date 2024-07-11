28448aa4a417587e7b3fbccdf3906e6010ca59a2
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-07-11 19:23:22 +0800

    remove unused regex

--- lib/CONST.ts
+++ lib/CONST.ts
@@ -357,14 +357,6 @@ const CONST = {
         MARKDOWN_EMAIL: EMAIL_BASE_REGEX,
 
         /**
-         * Regex matching an text containing an Emoji
-         *
-         * @type RegExp
-         */
-        // eslint-disable-next-line no-misleading-character-class
-        EMOJIS: /[\p{Extended_Pictographic}\u200d\u{1f1e6}-\u{1f1ff}\u{1f3fb}-\u{1f3ff}\u{e0020}-\u{e007f}\u20E3\uFE0F]|[#*0-9]\uFE0F?\u20E3/gu,
-
-        /**
          * Regex matching an text containing an Emoji that can be a single emoji or made up by some different emojis
          *
          * @type RegExp
