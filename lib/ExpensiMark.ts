919259b40335d401609edaf375353eb651868c23
Michał Skałka <michal.skalka@swmansion.com>
2025-01-15 10:08:26 +0100

    Fix problem with parsing empty inline code

--- lib/ExpensiMark.ts
+++ lib/ExpensiMark.ts
@@ -228,14 +228,14 @@ export default class ExpensiMark {
                 // At least one non-whitespace character or a specific whitespace character (" " and "\u00A0")
                 // must be present inside the backticks.
                 regex: /(\B|_|)&#x60;(.*?)&#x60;(\B|_|)(?!(?!<pre>)[^<]*(?:<(?!pre>)[^<]*)*<\/pre>|[^<]*<\/video>)/gm,
-                replacement: (_extras, _match, g1, g2, g3) => {
+                replacement: (_extras, match, g1, g2, g3) => {
                     const g2Value = g2.trim() === '' ? g2.replaceAll(' ', '&nbsp;') : g2;
                     if (!g2Value) {
-                        return _match;
+                        return match;
                     }
                     return `${g1}<code>${g2Value}</code>${g3}`;
                 },
-                rawInputReplacement: (_extras, _match, g1, g2, g3) => `${g1}<code>${g2}</code>${g3}`,
+                rawInputReplacement: (_extras, match, g1, g2, g3) => (g2 ? `${g1}<code>${g2}</code>${g3}` : match),
             },
 
             /**
