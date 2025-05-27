c478415c57ff2e8ab16432973eb3ed215af31b28
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-05-27 22:24:25 +0530

    convert fastmerge to worklets

--- lib/fastMerge.ts
+++ lib/fastMerge.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/prefer-for-of */
 
+'worklet';
+
 // Mostly copied from https://medium.com/@lubaka.a/how-to-remove-lodash-performance-improvement-b306669ad0e1
 
 /**
