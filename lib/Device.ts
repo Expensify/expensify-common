f221595ef66affed46e48bac7dc9255edd7aa227
dmkt9 <demoskratos9x@gmail.com>
2025-10-29 09:20:18 +0700

    Add support for detecting iOS 26

--- lib/Device.ts
+++ lib/Device.ts
@@ -12,6 +12,8 @@ function getOSAndName(): DeviceInfo {
     const {browser, os} = parser.getResult();
 
     let osVersion = os.version;
+    // Detection logic inspired by UAParser guide on iOS 26:
+    // https://docs.uaparser.dev/guides/how-to-detect-ios-26-using-javascript.html
     if (browser.name === 'Mobile Safari' && browser.major === '26' && os.name === 'iOS' && os.version === '18.6') {
         osVersion = '26';
     }
