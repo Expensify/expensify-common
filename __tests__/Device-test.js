08c7187f2f01caec24bfe65525593f794af2bc64
dmkt9 <demoskratos9x@gmail.com>
2025-11-07 22:15:07 +0700

    add test

--- __tests__/Device-test.js
+++ __tests__/Device-test.js
@@ -14,7 +14,8 @@ jest.mock('ua-parser-js', () => {
 const UA_IOS_18 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1';
 const UA_IOS_26_1 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Mobile/15E148 Safari/604.1';
 const UA_IOS_26_2 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Mobile/15E148 Safari/604.1';
-const UA_IOS_26_3 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_10 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.1.2 Mobile/15E148 Safari/604.1';
+const UA_IOS_26_3 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_7_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Mobile/15E148 Safari/604.1';
+const UA_IOS_26_4 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_10 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.1.2 Mobile/15E148 Safari/604.1';
 
 describe('getOSAndName', () => {
     it('should return iOS version 18', () => {
@@ -30,6 +31,9 @@ describe('getOSAndName', () => {
         expect(getOSAndName().osVersion).toEqual('26');
 
         mockUA = UA_IOS_26_3;
+        expect(getOSAndName().osVersion).toEqual('26');
+
+        mockUA = UA_IOS_26_4;
         expect(getOSAndName().osVersion).toEqual('26');
     });
 });
