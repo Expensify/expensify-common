3c2c93d6ae407e690d1aa58080618751cbcd6611
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-07-17 15:33:11 +0100

    add mergeHistoryByTimestamp

--- lib/ReportHistoryStore.jsx
+++ lib/ReportHistoryStore.jsx
@@ -172,6 +172,28 @@ export default class ReportHistoryStore {
     }
 
     /**
+     * Merges history items by reportActionID into the cache and creates it if it doesn't yet exist.
+     *
+     * @param {Number} reportID
+     * @param {Object[]} newHistory
+     */
+    mergeHistoryByTimestamp(reportID, newHistory) {
+        if (newHistory.length === 0) {
+            return;
+        }
+
+        const newCache = _.reduce(newHistory.reverse(), (prev, curr) => {
+            if (!_.findWhere(prev, {reportActionID: curr.reportActionID})) {
+                prev.unshift(curr);
+            }
+            return prev;
+        }, this.cache[reportID] || []);
+
+        // Sort items in case they have become out of sync
+        this.cache[reportID] = _.sortBy(newCache, 'reportActionTimestamp').reverse();
+    }
+
+    /**
      * Gets the history. This flow does not depend on the deprecated sequence number in report actions.
      *
      * @param {Number} reportID
